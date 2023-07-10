ef291bfdb8e2e05f96a10fbfc497f86d0c8d3133
nhadi44 <hadinurhidayat97@hotmail.com>
2023-07-11 05:42:17 +0700

    add title dynamic

--- src/Pages/Home/page.jsx
+++ src/Pages/Home/page.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react";
+
 export const HomePage = () => {
+  const [title, setTitle] = useState("Readme.md - Where good ideas find you");
+
+  useEffect(() => {
+    document.title = title;
+  }, []);
+
   return (
     <>
       <h1>Home Page</h1>
